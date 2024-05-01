f17f301df8beb16c50adf1d504cbe6110b9a607e
goutambhosagar <157117814+goutambhosagar@users.noreply.github.com>
2024-05-01 18:10:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.querySelectorAll('.navbar a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+
+
+
+
+
+
+
+
